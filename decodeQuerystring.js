671f6ccee8a48c2a4081147d8b73f9522c9bf72a
Alisdair Rankine <info@alisdairrankine.com>
2012-08-14 12:19:17 +0200

    Update decodeQuerystring.js
    allowed simple (1 dimension) arrays.

--- decodeQuerystring.js
+++ decodeQuerystring.js
@@ -1,14 +1,28 @@
-//simple URI query to JS Object decoder.
-function decodeQuerystring(string){
+function queryStringToObject(string) {
     var result = {};
-    var kvPairs = string.split(/&/g);
-    for (i in kvPairs){
-        var kvPair = kvPairs[i].split(/=/)
-        if(kvPair[1] === undefined){
-            kvPair[1] = true;
-        }
-        result[kvPair[0]] = decodeURIComponent(kvPair[1]);
+	var kvPairs = string.split(/&/g);
+	for (i in kvPairs) {
+		var kvPair = kvPairs[i].split(/=/)
+		if (kvPair[1] === undefined) {
+			kvPair[1] = true;
+		}
+		var key = decodeURIComponent(kvPair[0]);
+		var keySplit = key.split("[");
+		if (keySplit.length>1){
+			var keyRoot = keySplit.shift();
+			if (result[keyRoot] == undefined){
+				result[keyRoot] = [];
+			}
+			var shifted = keySplit.shift().replace("]","");
+			if (shifted!=""){
+				result[keyRoot][shifted] = kvPair[1];
+			} else{
+				result[keyRoot].push(kvPair[1]);
+			}
+		} else {
+			result[key] = decodeURIComponent(kvPair[1]);
+		}
 
-    }
-    return result;
+	}
+	return result;
 }
